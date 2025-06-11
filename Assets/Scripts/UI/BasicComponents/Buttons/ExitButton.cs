cf8e508da4af47cfc3ce948293a338f36ff15569
Pedrocasf <pedrostarling2000@gmail.com>
2025-06-11 14:50:22 -0300

    Retirar partes do projeto sem sentido

--- Assets/Scripts/UI/BasicComponents/Buttons/ExitButton.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class ExitButton : ButtonBehaivour
-{
-    protected override void OnClick()
-    {
-        Application.Quit();
-    }
-}
--- Assets/Scripts/UI/BasicComponents/Buttons/RetrySceneButton.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine.SceneManagement;
-
-public class RetrySceneButton : ButtonBehaivour
-{
-    protected override void OnClick()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-}
